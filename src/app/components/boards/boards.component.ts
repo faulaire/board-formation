d6a6c3b0e2bf8c3532f97049711e95e76687c75b
faulaire <fabien.aulaire@cfm.fr>
2019-07-01 17:12:26 +0200

    Material design for boards + new font

--- src/app/components/boards/boards.component.ts
+++ src/app/components/boards/boards.component.ts
@@ -10,12 +10,19 @@ import {Board} from '../../interfaces/board';
 })
 export class BoardsComponent implements OnInit {
 
-  public _boards : Board[];
+  public boards: Board[];
+  public columns = 3;
+  public rowHeight = '4:2';
+
   ngOnInit() {
     this.boardsService.load().subscribe((res: Board[]) => {
-      this._boards = res;
+      this.boards = res;
     });
   }
+
+  public add() {}
+  public edit(board: Board) {}
+  public delete(board: Board, index) {}
 
   constructor(private boardsService: BoardsService, private router: Router) { }
 }
